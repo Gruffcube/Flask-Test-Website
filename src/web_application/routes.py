6bbb6fddc43e09d00b114256b37344fb8a179740
Gruffcube <gruffcube01@gmail.com>
2026-06-16 12:23:06 +0100

    Made basic pure html website

--- src/web_application/routes.py
+++ src/web_application/routes.py
@@ -10,45 +10,47 @@ def index():
 
 
 
-@main_bp.route('/about')
-def about():
-    return render_template('about.html')
+@main_bp.route('/animal-information')
+def animals():
+    return render_template('animal_info.html')
 
 
 
-@main_bp.route('/lipsum')
-def lipsum():
-    return render_template('lipsum.html')
-
-
-
-# ~ @main_bp.route('/form')
-# ~ def form():
-    # ~ return render_template('form.html')
-
-
-
-@main_bp.route('/form', methods=['GET', 'POST'])
-def submit_task():
+@main_bp.route('/user-survey', methods=['POST', 'GET'])
+def survey():
     success = False
-
+    
     if request.method == 'POST':
-        # Get data from the form
-        fname = request.form.get('fname')
-        lname = request.form.get('lname')
-        age = request.form.get('age')
-        message = request.form.get('message')
-
-        # Here you can save it to a database, list, or file later
+        fname = request.form.get('fname', 'N/A')
+        lname = request.form.get('lname', 'N/A')
+        age = request.form.get('age', 'N/A')
+        email = request.form.get('email', 'N/A')
+        
+        gender = request.form.get('gender', 'N/A')
+        colour = request.form.get('colour', 'N/A')
+        marital_status = request.form.get('marital_status', 'N/A')
+        ethnicity = request.form.get('ethnicity', 'N/A')
+        
+        
+        padding = 30
         print(f"""
-    First Name ~ | ~ {fname}
-    Last Name ~~ | ~ {lname}
-    Age ~~~~~~~~ | ~ {age}
-    Message ~~~~ | ~ {message}
+    {'First Name':<{padding}} | {fname:<{padding}}
+    {'Last Name':<{padding}} | {lname:<{padding}}
+    {'Age':<{padding}} | {age:<{padding}}
+    {'Email':<{padding}} | {email:<{padding}}
+    
+    {'Gender':<{padding}} | {gender:<{padding}}
+    {'Favorite Colour':<{padding}} | {colour:<{padding}}
+    {'Maritial Status':<{padding}} | {marital_status:<{padding}}
+    {'Ethnicity':<{padding}} | {ethnicity:<{padding}}
         """)
-
+        
         success = True
         
-        return redirect(url_for('main.submit_task'))  # Refresh page
-
-    return render_template('form.html', success=success)
+        return redirect(url_for('main.survey', success=True))
+    
+    
+    if request.args.get('success') == 'True':
+            success = True
+    
+    return render_template('survey.html', success=success)
